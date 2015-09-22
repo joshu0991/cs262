503d454dad7b6d5ddbaf88ddbe323b57f19cd066
josh <joshualilly@me.com>
2015-09-22 14:13:43 -0400

    Added code

--- cs262/lab4/Lab4_jlilly3_204.c
+++ cs262/lab4/Lab4_jlilly3_204.c
@@ -4,32 +4,48 @@
 #define RNG_SEED 1467
 #define ARRAY_SIZE 25
 
-void randperm (int* a, const int n);
-void initilizeArray(int* numArray);
+void shuffleArray (int* a, const int n);
+void initilizeArray(int* numArray, const int);
 void printArray (int* numArray);
 
-void randperm (int* a, const int n)
+/*! Permute a random array
+ *  param[in] int* a pointer to our array
+ *  param[in] const int n the size of our array
+ */
+void shuffleArray (int* a, const int n)
 {
     int i, temp;
     long rand_number;
+    // Go from the top down to i = 1
     for (i = n - 1; i > 0; i--)
     {
+        // Generate random numbers 0 - 24 (array indecies
         rand_number = random() % (n);
+        // Swap array values
         temp = a[i];
         a[i] = a[rand_number];
         a[rand_number] = temp;
     }
 }
 
-void initilizeArray(int* numArray)
+/*!
+ * Init the given array with counting numbers
+ * param[in] int* numArray the array we will populate
+ */
+void initilizeArray(int* numArray, const int length)
 {
+    // Populate from zero up to array size 0 - 24
     int i;
-    for (i =0; i < ARRAY_SIZE; i++)
+    for (i =0; i < length; i++)
     {
         numArray[i] = i;
     }
 }
 
+/*!
+ * print the given array
+ * param[in] int* numArray the array to print
+ */
 void printArray (int* numArray)
 {
     int i;
@@ -48,12 +64,12 @@ int main()
             "My lab section is 204\n"
             "This program will shuffle an array of ints using Fisher-Yates Shuffle\n");
     srandom (RNG_SEED);
-    initilizeArray(numArray);
     int i;
-    for (i = 0; i < 1; i++)
+    for (i = 0; i < 10; i++)
     {
+        initilizeArray(numArray, ARRAY_SIZE);
         printArray (numArray);
-        randperm (numArray, ARRAY_SIZE);
+        shuffleArray (numArray, ARRAY_SIZE);
         printArray (numArray);
     }
     return 0;
